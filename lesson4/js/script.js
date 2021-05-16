f51278121127eb8b58cfad7a37012e3426fe818f
skeet1269 <jskelly@byui.edu>
2021-05-15 22:10:58 -0500

    commit

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,3 +1,13 @@
 function getdate() {
     document.getElementById("lastmodified").innerText =  "Last Modified: " + document.lastModified;
 }
+
+function togglemenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+const datefield = document.querySelector("date");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US" , { dateStyle: "full" }).format(now);
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
