0dc8024dca0b40c93aa4c971cebfda32a1c809b1
skeet1269 <jskelly@byui.edu>
2021-05-22 21:32:15 -0500

    commit

--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -10,4 +10,5 @@ const datefield = document.querySelector("date");
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US" , { dateStyle: "full" }).format(now);
 
+
 datefield.innerHTML = `<em>${fulldate}</em>`;
