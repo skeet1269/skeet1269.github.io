c56fd43c09a5b5c01bbf84e01dabc894ea2f2533
skeet1269 <jskelly@byui.edu>
2021-05-22 22:18:18 -0500

    commit

--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -12,3 +12,12 @@ const fulldate = new Intl.DateTimeFormat("en-US" , { dateStyle: "full" }).format
 
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
+
+let thedate = new Date ();
+
+    if (thedate.getDay() == 5) {
+    document.querySelector('#banner').style.display = "block";
+    }
+    else {
+       document.querySelector('#banner').style.display = 'none';
+    }
