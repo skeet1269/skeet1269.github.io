0000ac72ab57c4c946f1e702553b869193063b4f
skeet1269 <jskelly@byui.edu>
2021-06-06 00:54:11 -0500

    commit

--- lesson7/js/gallery.js
+++ lesson7/js/gallery.js
@@ -1,10 +1,12 @@
-let a = new Date();
-let b = new Date();
-localStorage.a = a;
-localStorage.b = b;
-a = Date.parse(localStorage.a);
-b = Date.parse(localStorage.b);
-let daysSinceLastVisit = b - a;
-console.log(daysSinceLastVisit);
-
-document.querySelector('#days').textContent = daysSinceLastVisit
+function getLastVisited() {
+    if (localStorage.lastVisited) {
+        const today = new Date();
+        const previous = localStorage.lastVisited;
+        const diffTime = Math.abs(today - previous);
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        document.getElementById('lastVisited').innerText = "You last visited " + diffDays +  " days ago.";
+    } else {
+        localStorage.lastVisited = new Date();
+        document.getElementById('lastVisited').innerText = '0';
+    }
+}
