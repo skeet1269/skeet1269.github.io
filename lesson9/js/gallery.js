3ebb4ac8c3cf13f0e9bee6e9bb7884eec8a2810e
skeet1269 <jskelly@byui.edu>
2021-06-14 23:13:58 -0500

    commit

---
+++ lesson9/js/gallery.js
@@ -0,0 +1,12 @@
+function getLastVisited() {
+    if (localStorage.lastVisited) {
+        const today = new Date();
+        const previous = localStorage.lastVisited;
+        const diffTime = Math.abs(today - previous);
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        document.getElementById('lastVisited').innerText = "You last visited " + diffDays +  " days ago.";
+    } else {
+        localStorage.lastVisited = new Date();
+        document.getElementById('lastVisited').innerText = "You last visited 0 days ago.";
+    }
+}
