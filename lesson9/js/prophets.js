c7ce754dfc4903537b3695c7bedb956662133c77
skeet1269 <jskelly@byui.edu>
2021-06-17 22:22:29 -0500

    commit

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -11,13 +11,24 @@ fetch(requestURL)
     for (let i = 0; i < prophets.length; i++ ) {
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
+      let dob = document.createElement('h3');
+      let dod = document.createElement('h3')
+      let pob = document.createElement('h3');
       let img = document.createElement('img');
 
+
       h2.textContent = `${prophets[i].name} ${prophets[i].lastname}`;
+      dob.textContent = "Date of Birth: " + ' ' + prophets[i].birthdate;
+      dod.textContent = "Date of Death: " + ' ' + prophets[i].death;
+      pob.textContent = "Place of Birth: " + ' ' + prophets[i].birthplace;
       img.setAttribute('src', prophets[i].imageurl);
-      img.setAttribute('alt', `The official portrait of ${prophets[i].name} ${prophets[i].lastname}!`)
+      img.setAttribute('alt', `The official portrait of ${prophets[i].name} ${prophets[i].lastname}!`);
+      
 
       card.append(h2);
+      card.append(dob);
+      card.append(dod);
+      card.append(pob);
       card.append(img);
       
 
