0b2a3584ae4080b7d159756d8d432fab76d61393
skeet1269 <jskelly@byui.edu>
2021-06-27 00:47:13 -0500

    commit

--- lesson10/js/preston5day.js
+++ lesson10/js/preston5day.js
@@ -6,16 +6,23 @@ fetch(apiURL)
     .then((jsObject) => {
         //console.log(jsObject);
         let day = 0;
-        const dayofWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        const dayofweek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-        const fiveDayForecast = jsObject.list.filter(forecast => forecast.dt_txt.includes("12:00:00"));
-
-        fiveDayForecast.forEach( x => {
-            let d = new Date(x.dt_txt);
-
-            document.getElementById(`dayofweek${day+1}`).textContent = dayofWeek[d.getDay()];
-            document.getElementById('forecast${day+1}').textContent = x.main.temp;
-        });
-
-    });
     
+    const fiveDayForecast = jsObject2.list.filter( forecast => forecast.dt_txt.includes('18:00:00'));
+
+    
+    fiveDayForecast.forEach( x => {
+      let d = new Date(x.dt_txt);
+      document.querySelector(`#day${day}`).textContent = dayofweek[d.getDay()];
+      document.querySelector(`#temp${day}`).textContent = x.main.temp.toFixed(0);
+      const imagesrc = 'https://openweathermap.org/img/wn/' + x.weather[0].icon + '.png';
+      const desc = x.weather[0].description;
+      document.querySelector(`#weathericon${day}`).setAttribute('src', imagesrc);
+      document.querySelector(`#weathericon${day}`).setAttribute('alt', desc);
+      day++
+    });
+  });
+    
+
+   
