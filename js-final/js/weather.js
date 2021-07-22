8f94685c3b2d37332a8308072f361fe447f06262
skeet1269 <jskelly@byui.edu>
2021-07-21 21:03:50 -0500

    commit

--- js-final/js/weather.js
+++ js-final/js/weather.js
@@ -1,12 +1,28 @@
-let lat = 33.29;
-let lon = 86.84;
-let appid = "5739b5e3d3fc1d5a757317dd290411fc";
-const apiURL = https://api.openweathermap.org/data/2.5/onecall?lat=33.29&lon=-86.84&exclude=hourly,daily&appid=5739b5e3d3fc1d5a757317dd290411fc
+const cityid = "4066811";
+const APPID = "5739b5e3d3fc1d5a757317dd290411fc"
+
+let apiURL = "https://api.openweathermap.org/data/2.5/weather?id=4066811&appid=5739b5e3d3fc1d5a757317dd290411fc&units=imperial";
 fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    //console.log(jsObject);
+    document.getElementById("Conditions").textContent = jsObject.weather[0].main;
+    document.getElementById("Current").textContent = Math.round(jsObject.main.temp);
+    document.getElementById("Humidity").textContent = jsObject.main.humidity;
+    
+  });
+
+  /*const lat = "33.29";
+  const lon = "86.84";
+  const APPID = "5739b5e3d3fc1d5a757317dd290411fc"
+  
+  let apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.29&lon=-86.84&exclude=hourly,daily&appid=739b5e3d3fc1d5a757317dd290411fc";
+  fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
-        console.log(jsObject);
-        let day = 0;
-        const dayofWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-    });
+      //console.log(jsObject);
+      document.getElementById("Conditions").textContent = jsObject.weather[0].main;
+      document.getElementById("Current").textContent = Math.round(jsObject.main.temp);
+      document.getElementById("Humidity").textContent = jsObject.main.humidity;
+      
+    });*/  
